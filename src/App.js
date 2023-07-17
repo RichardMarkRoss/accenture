37004151822ac695d5c98a9e615fbfd21d1051d7
richardr <richardr@wsb.co.za>
2023-07-17 08:17:30 +0200

    userLoggedIn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './components/login'
@@ -10,38 +9,31 @@ import Home from './scenes/home';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   useEffect(() => {
-    const userLoggedIn = // check user's login status, e.g., from local storage or API response
+    const userLoggedIn = isLoggedIn;
     setIsLoggedIn(userLoggedIn);
   }, []);
 
-  const ProtectedRoute = ({ element, path }) => {
-    if (isLoggedIn) {
-      return <Route path={path} element={element} />;
-    } else {
-      return <Navigate to="/sign-in" />;
-    }
-  };
-  // const dbConnect = require("./db/dbConnect");
-  // execute database connection 
-  // dbConnect();
-  function App() {
-    return (
-      <Router>
-        <div className="App">
-          <div className="auth-wrapper">
-            <div className="auth-inner">
-              <Routes>
-                <Route path="/sign-in" element={<Login />} />
-                <Route path="/sign-up" element={<SignUp />} />
-                <ProtectedRoute path="/" element={<Home />} />
-              </Routes>
-            </div>
+  return (
+    <Router>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              {isLoggedIn ? (
+                <Route path="/" element={<Home />} />
+              ) : (
+                <Navigate to="/sign-in" />
+              )}
+            </Routes>
           </div>
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
